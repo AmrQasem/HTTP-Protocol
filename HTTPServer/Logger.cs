88e95b43ea105ea803685e8f6cf31f947b45ffe4
Amr Ahmed Qasem <amrkasem47@yahoo.com>
2017-12-08 10:11:22 +0200

    First Commit in networks project

---
+++ HTTPServer/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HTTPServer
+{
+    class Logger
+    {
+        public static void LogException(Exception ex)
+        {
+            // TODO: Create log file named log.txt to log exception details in it
+            // for each exception write its details associated with datetime 
+        }
+    }
+}
